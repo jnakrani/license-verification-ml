ae23453257d6b46c8f861181eeb07262b4cabb56
jnakrani <jaydipnakrani888@gmail.com>
2023-07-31 18:03:12 +0530

    feat(setup): face verification - implemneted flask to serve upload endpoint - implemented deep face to detect face and predict age - implemented tesseract to extract dob from the images

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
 # license-verification-ml
+This Flask application allows users to verify their age and identity using two images: a license image and a selfie image. The application uses face detection and recognition techniques along with OCR (Optical Character Recognition) to extract the age from the selfie image and compare it with the age on the license image for verification. The similarity percentage between the faces in the two images is also calculated to assess the match.
+
+
+## Prerequisites
+Before running the application, make sure you have the following installed:
+> Python (version 3.9.1)
+> Tesseract OCR (install it separately and provide the path in the code)
+
+
+## Endpoints
+
+ - ##### Ping Endpoint
+  ```GET /ping```
+
+-- Use this endpoint to check if the server is running. It returns a simple success message.
+ - ##### Upload Endpoint
+ 
+  ```POST /upload```
+
+-- Use this endpoint to upload the "License_image" and "Selfie_image" files and get the results.
+
+- ##### Request
+- Method: POST
+- Headers: Content-Type: multipart/form-data
+- Body: Form-data
+  - Key: License_image, Type: File
+  - Key: Selfie_image, Type: File
+
+- ##### Response
+The API will process the uploaded images and return the following JSON response:
+```json
+{
+  "predicted_age_from_selfie_image": "30 year",
+  "calculated_age_from_license": "32 year",
+  "face_similarity_percentage": "80.50%"
+}
+```
+
+- `predicted_age_from_selfie_image`: The predicted age from the selfie image in years.
+- `calculated_age_from_license`: The calculated age from the license image in years.
+- `face_similarity_percentage`: The similarity percentage between the license image and the selfie image.
+
+## Usage
+
+1. Ensure you have Python and the required packages installed (Flask, deepface, pytesseract, etc.).
+
+2. Clone or download the project files to your local machine.
+
+3. Install the required packages using the following command:
+```
+pip install -r requirements.txt
+```
+
+4. Create a `.env` file in the project directory and set the `TESSERACT_PATH` variable to the path of your Tesseract OCR executable. For example:
+```
+TESSERACT_PATH=/path/to/tesseract
+```
+5. Run the Flask app:
+```
+python app.py
+```
+
+6. The app will be running on http://127.0.0.1:5000/.
+
+7. Open Postman or any other API client.
+
+8. Use the "ping" endpoint to check if the server is running by making a GET request to http://127.0.0.1:5000/ping.
+
+9. To use the "upload" endpoint, create a POST request to http://127.0.0.1:5000/upload.
+
+10. In the body of the request, choose the "form-data" option and add two keys:
+
+  - Key: license_image, Type: File (Select your license image file).
+  - Key: selfie_image, Type: File (Select your selfie image file).
+
+11. Send the request, and the API will process the images and return the JSON response with the face similarity and age information.
+
+12. You will see the logs in the api_log.log file located in the project directory.
+
+Note: Make sure the images you are uploading are in one of the allowed formats: png, jpg, jpeg, gif, jfif.
+
+## Important Notes
+
+- The API uses the deepface library to perform face recognition and age prediction.
+
+- It uses Tesseract OCR to recognize text from the "License_image" (if present) to calculate the age from the license.
+
+- The app saves the uploaded images temporarily in the static/uploaded_images folder. For production use, consider handling image storage differently.
+
+- Ensure you provide a secure secret key for the app (change the app.config['SECRET_KEY'] value) when deploying it to production.
+
+
+That's it! You can now use the API to analyze face similarity and age information from your uploaded images
---
+++ app.py
@@ -0,0 +1,175 @@
+import os
+from flask import Flask, request,jsonify
+from werkzeug.utils import secure_filename
+import cv2
+from deepface import DeepFace
+from dotenv import load_dotenv
+load_dotenv()
+import logging
+from datetime import datetime
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = os.getenv("TESSERACT_PATH") or r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+import re
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploaded_images'
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif', 'jfif'])
+app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a secure secret key
+
+def generate_dynamic_path(filename):
+    return os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+class ImageApp:
+    def __init__(self):
+        pass
+    def preprocess_image_and_recognize_text(self,filepath1):
+    # Load the image
+        selfie_image = cv2.imread(filepath1)
+
+    # Preprocess the image.
+        selfie_image = cv2.cvtColor(selfie_image, cv2.COLOR_BGR2GRAY)
+        selfie_image = cv2.threshold(selfie_image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+        # Recognize the text in the image.
+        text = pytesseract.image_to_string(selfie_image)
+        # text = "Your OCR text"
+        # logging.info(f"Recognized Text: {text}")
+        return text
+
+
+    def calculate_age_from_text(self,text):
+        dob_patterns = [
+            r'\d{4}-\d{2}-\d{2}',  # YYYY-MM-DD
+            r'\d{2}-\d{2}-\d{4}',  # DD-MM-YYYY
+            r'\d{2}-\d{2}-\d{2}',
+            r'\d{2}/\d{2}/\d{4}',  # DD/MM/YY
+            r'\d{2}/\d{2}/\d{2}'
+        ]
+        dob_keywords = [
+            'DOB:',
+            'Date of Birth:',
+            'Birthdate:',
+            'D of B:',
+            'D of B',
+            'DofB',
+            'D of B',
+            'vos:',
+            'pos\t ',
+            'BRO ',
+            'pos ',
+            'oof8 ',
+        ]
+
+        for dob_pattern in dob_patterns:
+            for dob_keyword in dob_keywords:
+                dob_match = re.search(rf'{dob_keyword} {dob_pattern}', text)
+                if dob_match:
+                    dob = dob_match.group(0).split(' ')[1]
+
+                    try:
+                        # Convert the DOB string to a datetime object
+                        try:
+                            dob_datetime = datetime.strptime(dob, '%d/%m/%Y')
+                        except ValueError:
+                            dob_datetime = datetime.strptime(dob, '%d-%m-%y')
+
+                        # Get the current date
+                        current_datetime = datetime.now()
+
+                        # Calculate the age
+                        age_ocr = current_datetime.year - dob_datetime.year
+                        
+
+                        # Adjust for cases where the birthday hasn't occurred this year yet
+                        if (current_datetime.month, current_datetime.day) < (dob_datetime.month, dob_datetime.day):
+                            age_ocr -= 1
+                            # logging.info(f"Calculated Age: {age_ocr}")
+                        return age_ocr
+                    except ValueError as e: 
+                        print("", e)
+                        return None
+        return None
+    
+
+    def process_images(self, filepath1, filepath2):
+            
+            image1=filepath1
+            image2=filepath2
+            Age=DeepFace.analyze(img_path = image2, 
+                    actions = ["age", "gender", "emotion", "race"])
+            predicted_age_text = Age[0]['age']
+            result = DeepFace.verify(img1_path=image1, img2_path=image2,distance_metric="cosine",model_name="VGG-Face")
+            # Calculate the similarity percentage
+            # threshold=result['threshold']  #if thresold> 0.4 (depend) value then it is not same person
+            similarity_percentage = (1.0 - result['distance']) * 100
+            logging.info(f"Predicted Age (DeepFace): {predicted_age_text}")
+            logging.info(f"Similarity Percentage (DeepFace): {similarity_percentage}")
+            return predicted_age_text, similarity_percentage
+
+    def upload(self):
+        image_app = ImageApp()
+        predicted_age_text = None
+        similarity_percentage = None
+        age_ocr = None
+
+        if 'License_image' not in request.files or 'Selfie_image' not in request.files:
+            return jsonify({"error": "Both 'License_image' and 'Selfie_image' files are required."}), 400
+
+        license_image = request.files['license_image']
+        selfie_image = request.files['selfie_image']
+
+        if license_image and allowed_file(license_image.filename) and selfie_image and allowed_file(selfie_image.filename):
+            # Generate dynamic file paths for saving the uploaded images
+            filename1 = secure_filename(license_image.filename)
+            filename2 = secure_filename(selfie_image.filename)
+            filepath1 = generate_dynamic_path(filename1)
+            filepath2 = generate_dynamic_path(filename2)
+
+            # Save the uploaded images to the designated paths
+            license_image.save(filepath1)
+            selfie_image.save(filepath2)
+
+            # Process the images and obtain results
+            predicted_age_text, similarity_percentage = image_app.process_images(filepath1, filepath2)
+            text = image_app.preprocess_image_and_recognize_text(filepath1)
+            age_ocr = image_app.calculate_age_from_text(text)
+
+            # Log the results
+            app.logger.info(f"Predicted Age (DeepFace): {predicted_age_text}")
+            app.logger.info(f"Age OCR (Tesseract): {age_ocr} Years")
+            app.logger.info(f"Similarity Percentage (DeepFace): {similarity_percentage}")
+            
+
+            # Return JSON response
+            response_data = {
+                "predicted_age_from_selfie_image": f"{predicted_age_text} year",
+                "calculated_age_from_license": f"{age_ocr} year",
+                "face_similarity_percentage": "{:.2f}%".format(similarity_percentage)
+             }
+
+            return jsonify(response_data), 200
+
+        # Return an error response if the request is not valid or any processing fails
+        return jsonify({"error": "Invalid request or processing failed."}), 400
+
+
+@app.route('/ping')
+def index():
+    return {"success": "True"}, 200
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    image_app = ImageApp()
+
+    return image_app.upload()
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='api_log.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    # Run the app in debug mode
+    app.run(debug=True)
+
